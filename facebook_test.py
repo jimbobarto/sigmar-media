b32614f528e651ad709edc1e7e3f377659cc867b
James Bartlett <jimbobarto@yahoo.co.uk>
2019-10-07 08:30:18 +0100

    removing badness

--- facebook_test.py
+++ facebook_test.py
@@ -1,9 +1,9 @@
 import oauth_utilities
 import facebook
 
-#credentials = oauth_utilities.getCredentials()
+credentials = oauth_utilities.getCredentials()
 
-graph = facebook.GraphAPI(access_token="EAAUoBjqafjgBANYWrG3CPYFvrzm41Tt0ZAnZCkqVEpKBD5XafmksTzdK03Hrysr5S2PZB5lxZCWDRx06kq31xZBwgVAfJ41q2ZCqsNtqUhzfZBYrYMho502r5M5dOtDKBB3ZAfdWU8r6U9B6llfmZCOZBbCosvnbrY30MZD")
+graph = facebook.GraphAPI(access_token=credentials['facebook_access_token'])
 print graph
 
 graph.put_object("me", "feed", message="Just testing, nothing to see here...")
