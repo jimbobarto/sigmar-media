ed0eb9aef556d27b1f9b5aeffcbaa9d50c4f90f6
James Bartlett <jimbobarto@yahoo.co.uk>
2019-10-10 22:10:35 +0100

    fiddling with facebook although this needs a valid app arrrggghhhh

--- facebook_test.py
+++ facebook_test.py
@@ -4,6 +4,6 @@ import facebook
 credentials = oauth_utilities.getCredentials()
 
 graph = facebook.GraphAPI(access_token=credentials['facebook_access_token'])
-print graph
+print(graph)
 
 graph.put_object("me", "feed", message="Just testing, nothing to see here...")
