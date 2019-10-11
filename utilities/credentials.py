c7174a3c04ebfdc1d78ef91001b2ee841b35335a
James Bartlett <jimbobarto@yahoo.co.uk>
2019-10-11 23:37:43 +0100

    removing old credentials.py as redundant

--- utilities/credentials.py
+++
@@ -1,8 +0,0 @@
-import json
-
-class Credentials:
-    def get_credentials(self):
-        with open('.credentials') as json_file:  
-            credentials = json.load(json_file)
-
-        return credentials
